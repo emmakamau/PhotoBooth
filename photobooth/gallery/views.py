7ab57720cc3262a4e56ee85f8cdf3687744476d8
EmmaK <emmaculatewkamau@gmail.com>
2022-05-29 18:45:35 +0300

    Add food and potrait views and templates

--- photobooth/gallery/views.py
+++ photobooth/gallery/views.py
@@ -8,9 +8,25 @@ def homepage(request):
     return render(request,'index.html')
 
 def travel(request):
-    travel_images = Image.objects.all()
+    travel_images = Image.objects.filter(image_category='0')
 
     context={
         'travel_images':travel_images
     }
     return render(request,'category/travel.html',context=context)
+
+def food(request):
+    food_images = Image.objects.filter(image_category='0')
+
+    context={
+        'food_images':food_images
+    }
+    return render(request,'category/food.html',context=context)
+
+def potraits(request):
+    potraits_images = Image.objects.filter(image_category='0')
+
+    context={
+        'potraits_images':potraits_images
+    }
+    return render(request,'category/potraits.html',context=context)
