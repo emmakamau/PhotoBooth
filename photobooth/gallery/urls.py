804c6d42830bbb3b2c251efb65b1c9efc4818b1b
EmmaK <emmaculatewkamau@gmail.com>
2022-05-29 18:48:42 +0300

    Define urls for food and potraits

--- photobooth/gallery/urls.py
+++ photobooth/gallery/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.homepage,name = 'homepage'),
-    path('travel/',views.travel,name = 'travel')
+    path('travel/',views.travel,name = 'travel'),
+    path('food/',views.food,name = 'food'),
+    path('potraits/',views.potraits,name = 'potraits')
 ]
