5dde85a142c65cc80452ce57fa31b60982977696
EmmaK <emmaculatewkamau@gmail.com>
2022-05-29 17:17:02 +0300

    Define view that displays images of travel category

--- photobooth/gallery/views.py
+++ photobooth/gallery/views.py
@@ -1,5 +1,16 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
+
+def travel(request):
+    travel_images = Image.objects.filter(category='travel')
+
+    context={
+        'travel_images':travel_images
+    }
+    return render(request,'category/travel.html',context=context)
