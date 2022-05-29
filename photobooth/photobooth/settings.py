9d82754a60ed4d315d886ec7aef41e40757c049d
EmmaK <emmaculatewkamau@gmail.com>
2022-05-29 15:35:24 +0300

    Define homepage url and add navbar

--- photobooth/photobooth/settings.py
+++ photobooth/photobooth/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery',
+    'bootstrap4',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
