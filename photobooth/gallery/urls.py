a3e68c53cc34811acd0fa855d342a758d2de85bf
EmmaK <emmaculatewkamau@gmail.com>
2022-06-03 06:59:00 +0300

    DAdd a modal to the image

--- photobooth/gallery/urls.py
+++ photobooth/gallery/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.homepage,name = 'homepage'),
-    path('travel/',views.travel,name = 'travel'),
-    path('food/',views.food,name = 'food'),
-    path('potraits/',views.potraits,name = 'potraits')
+    path('gallery/',views.gallery,name = 'gallery')
 ]
--- photobooth/gallery/views.py
+++ photobooth/gallery/views.py
@@ -7,26 +7,9 @@ from .models import *
 def homepage(request):
     return render(request,'index.html')
 
-def travel(request):
-    travel_images = Image.objects.filter(image_category='1')
-
+def gallery(request):
+    gallery = Image.objects.all()
     context={
-        'travel_images':travel_images
+        'gallery':gallery
     }
-    return render(request,'category/travel.html',context=context)
-
-def food(request):
-    food_images = Image.objects.filter(image_category='2')
-
-    context={
-        'food_images':food_images
-    }
-    return render(request,'category/food.html',context=context)
-
-def potraits(request):
-    potraits_images = Image.objects.filter(image_category='3')
-
-    context={
-        'potraits_images':potraits_images
-    }
-    return render(request,'category/potraits.html',context=context)
+    return render(request,'category/gallery.html',context=context)
