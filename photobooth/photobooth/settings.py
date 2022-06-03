3e273debf984a2bb4420e9d57af76ed880e48c59
EmmaK <emmaculatewkamau@gmail.com>
2022-06-03 15:45:13 +0300

    Deploy

--- photobooth/photobooth/settings.py
+++ photobooth/photobooth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['photoboothemz.herokuapp.com','127.0.0.1']
 
