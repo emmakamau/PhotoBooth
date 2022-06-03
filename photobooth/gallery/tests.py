f8ee8e24ed36f86ba538ba7fd456e2e1a10d67fc
EmmaK <emmaculatewkamau@gmail.com>
2022-06-03 13:39:11 +0300

    Define test for category model

--- photobooth/gallery/tests.py
+++ photobooth/gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class TestCategory(TestCase):
+    def setUp(self) -> None:
+        self.example_category = Category(name="New Category")
+
+    def test_category_instance(self):
+        self.assertTrue(isinstance(self.example_category, Category))
